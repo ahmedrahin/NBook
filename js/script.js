d993ef0efb5fb2c5adb7d6dc818c6199e682ae3e
Rahin Ahmed <110545190+ahmedrahin@users.noreply.github.com>
2022-08-25 00:22:42 +0600

    NBook

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+$(".owl-carousel").owlCarousel({
+    center: true,
+    items: 1,
+    loop: true,
+    nav: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+  
